cb1f34c850c55a561819ca1c9636b84add769a06
TorrieL <hmminicity2@163.com>
2019-08-05 23:06:59 +0800

    create AddSmoothie compo& update router link

--- monkey-smoothies/src/components/Navbar.vue
+++ monkey-smoothies/src/components/Navbar.vue
@@ -2,11 +2,11 @@
   <div class="navbar">
     <nav class="nav-extended green darken-3">
       <div class="nav-content">
-        <router-link to="">
+        <router-link :to="{name: 'Index'}">
           <span class="nav-title amber-text text-lighten-2">Monkey Smoothies</span>
         </router-link>
         <a href="" class="btn-floating btn-large halfway-fab purple accent-3">
-          <router-link to="">
+          <router-link :to="{name:'AddSmoothie'}">
             <i class="material-icons">add</i>
           </router-link>
         </a>
