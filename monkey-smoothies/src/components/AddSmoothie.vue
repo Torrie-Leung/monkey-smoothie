38e6f756a2e21fa9ec5abb422126372df58cfc1e
TorrieL <hmminicity2@163.com>
2019-08-06 20:42:24 +0800

    add ingredients

--- monkey-smoothies/src/components/AddSmoothie.vue
+++ monkey-smoothies/src/components/AddSmoothie.vue
@@ -1,15 +1,60 @@
 <template>
-  <h2>Add Smoothie</h2>
+  <div class="add-smoothie container">
+    <h2 class="center-align indigo-tex">Add New Smoothie Recipe</h2>
+    <form @submit.prevent="AddSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie Title:</label>
+        <input type="text" name="title" v-model="title">
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">add an ingredient:</label>
+        <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
+      </div>
+      <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{feedback}}</p>
+        <button class="btn purple accent-4">Add Smoothie</button>
+      </div>
+    </form>
+  </div>
 </template>
 <script>
 export default {
   name: 'AddSmoothie',
   data(){
-    return{}
+    return{
+      title: null,
+      another: null,
+      ingredients:[],
+      feedback:null
+    }
   },
-  methods:{}
+  methods:{
+    AddSmoothie(){
+      console.log(this.title,this.ingredients)
+    },
+    addIng(){
+      if(this.another){
+        this.ingredients.push(this.another)
+        this.another = null
+        this.feedback = null
+      }else {
+        this.feedback = 'you must enter a value to add an ingredient'
+      }
+    }
+  }
 }
 </script>
 <style>
-
+.add-smoothie{
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+.add-smoothie h2{
+  font-size: 2em;
+  margin: 20px auto;
+}
+.add-smoothie .field{
+  margin: 20px auto;
+}
 </style>
